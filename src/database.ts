7d675b1acafabd9aa59fe65e1cdddd239a2afe71
DianaEspitia <78936469+DianaEspitia@users.noreply.github.com>
2021-12-23 10:50:01 -0500

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 export async function startConn() {
   // Cambiar la ruta y el puerto
-  await connect("mongodb://19151926-us-south.lb.appdomain.cloud:27017/clientsdb", {
+  await connect("mongodb://a03f9687-us-south.lb.appdomain.cloud:27017/clientsdb", {
     useNewUrlParser: true,
     useFindAndModify: false,
   });
